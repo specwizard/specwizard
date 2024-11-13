d0fe0d9f0e89f0577f84edf9b9ebccc3e0e726d1
Andres <andresaramburo27@gmail.com>
2024-11-13 10:36:16 +0100

    more documentation

--- specwizard/SpecWizard_Elements.py
+++ specwizard/SpecWizard_Elements.py
@@ -9,20 +9,24 @@ import h5py
 ElementNames = ['Hydrogen', 'Helium', 'Carbon', 'Oxygen', 'Silicon', 'Iron']
 class Elements:
     """
-	This class sets the physical parameters for the desired chemical elements that are needed\
-        to be able to compute the properties of its absorption lines"
-        This class reads properties of transitions from the atom.dat file that comes with VpFit.
-        
-        Parameters
-        ----------    
-    
-           atomfile  : str
-                hdf5 ile that was generated with SpecWizard_atomfile. (default = 'atome_dat.hdf5')
+    This class sets the physical parameters for desired chemical elements needed
+    to compute the properties of their absorption lines. It reads properties of transitions 
+    from an atom data file compatible with VpFit.
+
+    Args:
+        atomfile (str, optional): Path to an HDF5 file generated with SpecWizard_atomfile,
+            containing physical parameters of ions and elements. Defaults to 'atom_dat.hdf5'.
     """
     
     def __init__(self, 
                  atomfile="atom_dat.hdf5"):
+        """
+        Initialize the Elements class with a file containing element and ion parameters.
         
+        Args:
+            atomfile (str, optional): Path to the HDF5 file with atomic data for elements.
+                Defaults to 'atom_dat.hdf5'.
+        """       
         # file containing physical parameters of ions and elements
         self.atomdat       = atomfile
 
@@ -38,15 +42,27 @@ class Elements:
         
     def ElementParameters(self, ElementNames=ElementNames):
         """ 
-	For all desired element, read desired values from atom.dat
-         The structure returned is of the form
-        (Name, atomic weight, (ionization states))
-        Name is the name of the element, e.g. Hydrogen
-        atomic weight is the mass of the element in amu (isotopes are not accounted for)
-        (ionization stages) is a tuple, containing the considered ionisation states, e.g. (0) for HI, or (0,1) for HeI and HeII
-        lines: for each ionization stage, a list of tuples, containing wavelength in Angstrom, and f-value
-               these are read from Vpfit's "atom.dat"
-        """
+        Retrieve parameters for all specified elements from the atom data file.
+        
+        Args:
+            ElementNames (list of str): List of element names for which to retrieve parameters.
+                Each element name should correspond to an entry in the atom data file.
+        
+        Returns:
+            dict: A dictionary containing element properties in the form:
+                {
+                    'Name': str,                # Name of the element (e.g., 'Hydrogen')
+                    'atomic_weight': float,     # Atomic weight in atomic mass units (amu)
+                    'ionization_states': tuple, # Ionization states as integers (e.g., (0) for HI)
+                    'lines': list of tuples     # List of tuples for each ionization stage, containing:
+                                                # - wavelength in Angstroms (float)
+                                                # - f-value for transition (float)
+                }
+
+        Note:
+            The method reads data from the atom data file compatible with VpFit's "atom.dat" structure.
+            It includes details such as wavelengths and f-values for each ionization state.
+            """
         
         elementnames = ElementNames
         parameters   = {}
@@ -54,6 +70,7 @@ class Elements:
         IonState     = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 
                         'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII',
                        'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI']
+        # We read the atom hdf5 file
         atomfile = h5py.File(self.atomdat ,'r')
         for element in elementnames:
             try:
--- specwizard/SpecWizard_Input.py
+++ specwizard/SpecWizard_Input.py
@@ -1,94 +1,131 @@
-
 import numpy as np
 import glob
 import h5py
 from .Phys import ReadPhys
 from .SpecWizard_Elements import Elements
-from  .reading_simulations import *
+from .reading_simulations import *
 from .SimulationInputKeys import get_simkeys
 
-# physical constants in cgs units
-constants  = ReadPhys()
-
+# Physical constants in CGS units
+constants = ReadPhys()
 
 wizard = {}
 
 class ReadData:
-    '''
-    Read SPH particles from Eagle (Gadget), Hydrangea(C-Eagle) or Swift output
-    input: Wizardparamas(output from running SpecWizard_BuildInput)
-    '''
+    """
+    This class reads SPH particle data from simulation outputs such as Eagle (Gadget),
+    Hydrangea (C-Eagle), or Swift. It requires `Wizard` dictionary, generated from running
+    `SpecWizard_BuildInput`, to configure its input parameters.
+
+    Args:
+        wizard (dict): A dictionary containing all required simulation and file 
+            parameters for data reading, snapshot configurations, and additional settings. 
+
+    Attributes:
+        wizard (dict): Configuration parameters for the simulation.
+        fdir (str): Directory of the snapshot files.
+        fname (str): Full path to the snapshot file.
+        simtype (str): Type of simulation (e.g., "eagle", "swift").
+        snaptype (str): Snapshot type as specified in wizard.
+        readIonFrac (bool): Whether to read ion fraction information.
+        groupdic (dict): Dictionary of group configurations for the snapshot type.
+        groupname (str): Group name for accessing data in the simulation file.
+        header (dict): Header information from the simulation snapshot.
+        ToCGS (function): Conversion function for CGS units.
+        Hubble (function): Hubble parameter based on input configuration.
+    """
+    
     def __init__(self, wizard=wizard):
+        """
+        Initialize the ReadData class with a specified `wizard` dictionary.
+
+        Args:
+            wizard (dict): Dictionary of simulation parameters for reading data. 
+        """
         self.wizard = wizard
-        self.fdir       = wizard["snapshot_params"]["directory"]
-        self.fname      = self.fdir + '/' + wizard["snapshot_params"]["file"]
-        self.simtype    = wizard["file_type"]["sim_type"]
-        self.snaptype   = wizard["file_type"]["snap_type"]
-        self.readIonFrac = wizard['extra_parameters']['ReadIonFrac']['ReadIonFrac'] 
-        sim_keys  = get_simkeys(self.simtype)
-        groupdic  = sim_keys[self.snaptype]
+        self.fdir = wizard["snapshot_params"]["directory"]
+        self.fname = self.fdir + '/' + wizard["snapshot_params"]["file"]
+        self.simtype = wizard["file_type"]["sim_type"]
+        self.snaptype = wizard["file_type"]["snap_type"]
+        self.readIonFrac = wizard['extra_parameters']['ReadIonFrac']['ReadIonFrac']
+        sim_keys = get_simkeys(self.simtype)
+        groupdic = sim_keys[self.snaptype]
         self.groupdic = groupdic
         groupname = groupdic['groupname']
         self.groupname = groupname    
         self.wizard['short-LOS'] = False
         self.header = self.read_header()
-        inputfunc   = InputFunctions(fileparams=self.wizard,header=self.header)
-        self.ToCGS  = inputfunc.ToCGS
+        inputfunc = InputFunctions(fileparams=self.wizard, header=self.header)
+        self.ToCGS = inputfunc.ToCGS
         self.Hubble = inputfunc.Hubble
 
-
     def read_header(self):
+        """
+        Read and return header information from the simulation snapshot based on its type.
+
+        Returns:
+            dict: A dictionary containing header information of the simulation snapshot.
+
+        Raises:
+            ValueError: If the simulation type is not supported.
+        """
         simtype = self.simtype
         if simtype == "eagle":            
-            eagle               =  ReadEagle(self.wizard)
-            header         =  eagle.read_header()
+            eagle = ReadEagle(self.wizard)
+            header = eagle.read_header()
  
-        elif (simtype == "swift") or (simtype == "colibre"):
-        
-            swift               =  ReadSwift(self.wizard)
-            header         =  swift.read_header()
+        elif simtype in ["swift", "colibre"]:
+            swift = ReadSwift(self.wizard)
+            header = swift.read_header()
 
         elif simtype == "illustris":
-              
-            illustris           =  ReadIllustris(self.wizard)
-            header         =  illustris.read_header()
+            illustris = ReadIllustris(self.wizard)
+            header = illustris.read_header()
 
         elif simtype == "hydrangea":
-              
-            hydrangea           =  ReadHydrangea(self.wizard)
-            header         =  hydrangea.read_header()
+            hydrangea = ReadHydrangea(self.wizard)
+            header = hydrangea.read_header()
 
         else:
-            print("Simulation not supported!")         
-
+            raise ValueError("Simulation not supported!")
+        
         return header
+
     def read_particles(self):
+        """
+        Read particle data from the simulation snapshot and return sightline, particle, 
+        and header information.
+
+        Returns:
+            dict: A dictionary with keys:
+                - 'SightInfo' (dict): Information about the sightline.
+                - 'Particles' (dict): Particle data from the simulation.
+                - 'Header' (dict): Header information from the simulation snapshot.
+
+        Raises:
+            ValueError: If the simulation type is not supported.
+        """
         simtype = self.simtype
         if simtype == "eagle":            
-            eagle               =  ReadEagle(self.wizard)
-            particles,sightline =  eagle.read_particles()
+            eagle = ReadEagle(self.wizard)
+            particles, sightline = eagle.read_particles()
  
-        elif (simtype == "swift") or (simtype == "colibre"):
-        
-            swift               =  ReadSwift(self.wizard)
-            particles,sightline =  swift.read_particles()
+        elif simtype in ["swift", "colibre"]:
+            swift = ReadSwift(self.wizard)
+            particles, sightline = swift.read_particles()
 
         elif simtype == "illustris":
-              
-            illustris           =  ReadIllustris(self.wizard)
-            particles,sightline =  illustris.read_particles()
+            illustris = ReadIllustris(self.wizard)
+            particles, sightline = illustris.read_particles()
 
         elif simtype == "hydrangea":
-              
-            hydrangea           =  ReadHydrangea(self.wizard)
-            particles,sightline =  hydrangea.read_particles()
+            hydrangea = ReadHydrangea(self.wizard)
+            particles, sightline = hydrangea.read_particles()
 
         else:
-            print("Simulation not supported!") 
+            raise ValueError("Simulation not supported!")
 
-            
-        
         self.wizard['sightline'] = sightline
         self.wizard['Header'] = self.header
 
-        return {'SightInfo': sightline, 'Particles' : particles, 'Header': self.header}
+        return {'SightInfo': sightline, 'Particles': particles, 'Header': self.header}
